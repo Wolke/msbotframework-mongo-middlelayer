46ed21b26342f863606dff490d97b1b2a0568d78
Wolke <wolkesau@gmail.com>
2018-02-06 11:59:56 +0800

    next add npm publish

--- app.js
+++ app.js
@@ -1,59 +1,50 @@
-var restify = require('restify');
-var builder = require('botbuilder');
-
-var istorage= require('./lib/IStorageClient');
-var azure = require('./lib/AzureBotStorage.js');
-var conf = require('./config/conf.js');
-
-//=========================================================
-// Bot Setup
-//=========================================================
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3980, function () {
-   console.log('%s listening to %s', server.name, server.url);
-});
-
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId:conf.bot.appId,
-    appPassword:conf.bot.appPass
-});
-
-
-
-// Listen for messages from users 
-server.post('/api/messages', connector.listen());
-
-
-//Store session and context into mnongodb
-var docDbClient = new istorage.IStorageClient();
-var tableStorage = new azure.AzureBotStorage({ gzipData: false },docDbClient);
-var bot = new builder.UniversalBot(connector).set('storage', tableStorage);//set your storage here
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Storage = require("./index");
+var builder = require("botbuilder");
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector)
+    .set("storage", new Storage.default({
+    DatabaseName: "test",
+    mongoIp: "127.0.0.1",
+    mongoPort: "27017",
+    collectionName: "gogo"
+}, {
+    gzipData: false
+}));
 bot.use(builder.Middleware.dialogVersion({ version: 3.0, resetCommand: /^reset/i }));
-
-
-bot.dialog('/', [
-    function (session, args, next) {
-        if (!session.userData.name) {
-            session.beginDialog('/profile');
-        } else {
-            next();
-        }
+bot.dialog("/", [
+    function (s) { builder.Prompts.text(s, "name?"); },
+    function (s, r) {
+        s.userData.name = r.response;
+        console.log("after name", s.userData);
+        builder.Prompts.number(s, "age?");
     },
-    function (session, results) {
-        session.send('Hello %s!', session.userData.name);
+    function (s, r) {
+        console.log("after age", s.userData);
+        s.userData.age = r.response;
+        s.endDialog("bady " + s.userData.name);
     }
 ]);
-
-bot.dialog('/profile', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    function (session, results) {
-        console.log("results.response==>",results.response);
-        session.userData.name = results.response;
-        session.endDialog();
-    }
-]);
+// bot.dialog('/', [
+//     function (session: builder.Session, args: any, next: any) {
+//         if (!session.userData.name) {
+//             session.beginDialog('/profile');
+//         } else {
+//             next();
+//         }
+//     },
+//     function (session, results) {
+//         session.send('Hello %s!', session.userData.name);
+//     }
+// ]);
+// bot.dialog('/profile', [
+//     function (session) {
+//         builder.Prompts.text(session, 'Hi! What is your name?');
+//     },
+//     function (session, results) {
+//         console.log("results.response==>", results.response);
+//         session.userData.name = results.response;
+//         session.endDialog();
+//     }
+// ]);
---
+++ app.ts
@@ -0,0 +1,59 @@
+import * as Storage from "./index"
+import * as builder from "botbuilder"
+
+let connector = new builder.ConsoleConnector().listen();
+
+
+var bot = new builder.UniversalBot(connector)
+    .set("storage", new Storage.default({
+        DatabaseName: "test",
+        mongoIp: "127.0.0.1",
+        mongoPort: "27017",
+        collectionName: "gogo"
+    }, {
+            gzipData: false
+        }));
+
+bot.use(builder.Middleware.dialogVersion({ version: 3.0, resetCommand: /^reset/i }));
+
+bot.dialog("/", [
+    s => { builder.Prompts.text(s, "name?") },
+    (s, r) => {
+        s.userData.name = r.response;
+        console.log("after name",s.userData)
+        builder.Prompts.number(s, "age?")
+    },
+    (s, r) => { 
+        console.log("after age",s.userData)
+    
+        s.userData.age = r.response
+        
+        s.endDialog("bady " + s.userData.name) 
+    }
+
+])
+
+
+// bot.dialog('/', [
+//     function (session: builder.Session, args: any, next: any) {
+//         if (!session.userData.name) {
+//             session.beginDialog('/profile');
+//         } else {
+//             next();
+//         }
+//     },
+//     function (session, results) {
+//         session.send('Hello %s!', session.userData.name);
+//     }
+// ]);
+
+// bot.dialog('/profile', [
+//     function (session) {
+//         builder.Prompts.text(session, 'Hi! What is your name?');
+//     },
+//     function (session, results) {
+//         console.log("results.response==>", results.response);
+//         session.userData.name = results.response;
+//         session.endDialog();
+//     }
+// ]);
